6d03bbc8b8ce2aa873cdcac5945bb1fd2edced14
이승환 <lsh01087674117@gmail.com>
2023-05-16 17:39:13 +0900

    주석추가.

--- src/chap02/section1/UserClassImport.kt
+++ src/chap02/section1/UserClassImport.kt
@@ -7,8 +7,8 @@ import com.example.edu.Person as User
  * @since 2023-05-16
  */
 fun main() {
-    val user1 = User("이승환", 31)
-    val user2 = Person("lsh955", "이승환")
+    val user1 = User("이승환", 31) // import com.example.edu.Person 이 User 로 대체.
+    val user2 = Person("lsh955", "이승환") // 이 파일 안에 있는 Person 클래스의 객체 생성.
 
     println(user1.name)
     println(user1.age)
