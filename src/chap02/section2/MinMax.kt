ca185cf04c1c04d475d9c456fc4169c21879ea2d
이승환 <lsh01087674117@gmail.com>
2023-05-17 08:40:53 +0900

    정수 자료형과 실수 자료형의 최솟값 및 최대값 출력하기.

---
+++ src/chap02/section2/MinMax.kt
@@ -0,0 +1,18 @@
+package chap02.section2
+
+/**
+ * @author 이승환
+ * @since 2023-05-17
+ */
+fun main() {
+    println("Byte min: " + Byte.MIN_VALUE + " max: " + Byte.MAX_VALUE)
+    println("Short min: " + Short.MIN_VALUE + " max: " + Short.MAX_VALUE)
+    println("Int min: " + Int.MIN_VALUE + " max: " + Int.MAX_VALUE)
+    println("Long min: " + Long.MIN_VALUE + " max: " + Long.MAX_VALUE)
+    println("Float min: " + Float.MIN_VALUE + " max: " + Float.MAX_VALUE)
+    println("Double min: " + Double.MIN_VALUE + " max: " + Double.MAX_VALUE)
+
+    var a: Byte = 127
+    a = (a + 2).toByte()
+    println(a)
+}
