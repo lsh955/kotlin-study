0db450ae1819a2222e8b53ea8691e7f70eb04b77
이승환 <lsh01087674117@gmail.com>
2023-05-16 17:34:25 +0900

    클래스 가져오기.

---
+++ src/chap02/section1/UserClassImport.kt
@@ -0,0 +1,14 @@
+package chap02.section1
+
+import com.example.edu.Person
+
+/**
+ * @author 이승환
+ * @since 2023-05-16
+ */
+fun main() {
+    val user1 = Person("이승환", 31)
+
+    println(user1.name)
+    println(user1.age)
+}
