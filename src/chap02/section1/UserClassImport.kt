e231bbd7caeccc582d49230ab87ffce4a94a0c66
이승환 <lsh01087674117@gmail.com>
2023-05-16 17:38:11 +0900

    같은 이름의 클래스가 있는 경우의 처리상황.

--- src/chap02/section1/UserClassImport.kt
+++ src/chap02/section1/UserClassImport.kt
@@ -1,14 +1,20 @@
 package chap02.section1
 
-import com.example.edu.Person
+import com.example.edu.Person as User
 
 /**
  * @author 이승환
  * @since 2023-05-16
  */
 fun main() {
-    val user1 = Person("이승환", 31)
+    val user1 = User("이승환", 31)
+    val user2 = Person("lsh955", "이승환")
 
     println(user1.name)
     println(user1.age)
+
+    println(user2.id)
+    println(user2.name)
 }
+
+class Person(val id: String, val name: String)
