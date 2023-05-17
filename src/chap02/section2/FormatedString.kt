a500ad6c2b3e108f701c30740f87621cbb86a5f4
이승환 <lsh01087674117@gmail.com>
2023-05-17 10:50:48 +0900

    줄바꿈/탭/공백 문자열 처리를 하기위한, 형식화된 다중 문자열 출력.

---
+++ src/chap02/section2/FormatedString.kt
@@ -0,0 +1,16 @@
+package chap02.section2
+
+/**
+ * @author 이승환
+ * @since 2023-05-17
+ */
+fun main() {
+    val num = 10
+    val formattedString = """
+       var a = 6
+       var b = "Kotlin"
+       println(a + num)
+    """
+
+    println(formattedString)
+}
