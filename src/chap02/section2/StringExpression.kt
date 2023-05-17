dc78c3224b33a45ba48a57d4f45aeba87c3ef05c
이승환 <lsh01087674117@gmail.com>
2023-05-17 10:41:59 +0900

    주석보강 및 \"을 사용해 큰따옴표를 문자열 추가.

--- src/chap02/section2/StringExpression.kt
+++ src/chap02/section2/StringExpression.kt
@@ -8,6 +8,8 @@ fun main() {
     var a = 1
     val str1 = "a = $a" // String 자료형의 s1 을 선언하고 초기화, 변수 a가 사용됨.
     val str2 = "a = ${a + 2}" // 문자열에 표현식 사용.
-
     println("str1: \"$str1\", str2: \"$str2\"")
+
+    val special = "\"hello\", I have \$15" // 이스케이프 문자인 \"을 사용해 큰따옴표를 문자열 안에 표현.
+    println(special)
 }
