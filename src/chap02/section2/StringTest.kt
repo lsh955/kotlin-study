361cc19728e31579e96462f6e53a4006e0cd3644
이승환 <lsh01087674117@gmail.com>
2023-05-17 10:28:32 +0900

    주석보강.

--- src/chap02/section2/StringTest.kt
+++ src/chap02/section2/StringTest.kt
@@ -10,5 +10,10 @@ fun main() {
     var str3 = "Hello"
 
     println("str1 === str2: ${str1 === str2}") // false
+
+    /**
+     * 코틀린은 String Pool 이라는 공간에 문자열인 "Hello"를 저장해 두고,
+     * str1, str3 을 참조하도록 만든다.
+     */
     println("str1 === str3: ${str1 === str3}") // true
 }
