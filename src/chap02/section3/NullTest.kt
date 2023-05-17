6cc8d953255b0f093196523d28a522866b6e0ea8
이승환 <lsh01087674117@gmail.com>
2023-05-17 19:09:35 +0900

    null Test.

---
+++ src/chap02/section3/NullTest.kt
@@ -0,0 +1,12 @@
+package chap02.section3
+
+/**
+ * @author 이승환
+ * @since 2023-05-17
+ */
+fun main() {
+    var str1: String = "Hello Kotlin"
+    // str1 = null 코틀린은 null을 허용하지 않음.
+
+    println("str1: $str1")
+}
