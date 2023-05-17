a59d8fcc4dd7fc9bfd4bf180adbaf447802aafbd
이승환 <lsh01087674117@gmail.com>
2023-05-17 19:11:45 +0900

    null 처리하기.

--- src/chap02/section3/NullTest.kt
+++ src/chap02/section3/NullTest.kt
@@ -7,6 +7,9 @@ package chap02.section3
 fun main() {
     var str1: String = "Hello Kotlin"
     // str1 = null 코틀린은 null을 허용하지 않음.
-
     println("str1: $str1")
+
+    var str2: String? = "이승환"
+    str2 = null // null을 허용하려면 자료형에 ? 기호를 명시해야 한다.
+    println("str2: $str2")
 }
