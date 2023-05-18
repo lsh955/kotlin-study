edb811905a61adef9c05a40fe33944fcc1b25ed3
이승환 <lsh01087674117@gmail.com>
2023-05-18 17:49:29 +0900

    참조에 의한 호출 방식으로 일반함수 호출 구현.

---
+++ src/chap03/section3/FunctionReference.kt
@@ -0,0 +1,26 @@
+package chap03.section3
+
+/**
+ * @author 이승환
+ * @since 2023-05-18
+ */
+fun main() {
+    val res1 = funcParam(3, 2, ::sum) // :: 기호를 함수이름에 사용해, 소괄호 인자를 생략가능.
+    println(res1) // 5
+
+    hello(::text) // 최종, return 이 없으므로 반환값이 없음.
+
+    val likeLambda = ::sum
+    println(likeLambda(6, 6))
+}
+
+fun sum(a: Int, b: Int) = a + b
+fun text(a: String, b: String) = "Hi! $a $b"
+
+fun funcParam(a: Int, b: Int, c: (Int, Int) -> Int): Int {
+    return c(a, b)
+}
+
+fun hello(body: (String, String) -> String): Unit {
+    println(body("Hello", "World"))
+}
