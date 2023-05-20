feccd5de36bf50dc89cecb7fe445d223df3d029d
이승환 <lsh01087674117@gmail.com>
2023-05-20 11:14:35 +0900

    인라인 함수 작성해 보기.

---
+++ src/chap03/section5/InlineFunction.kt
@@ -0,0 +1,17 @@
+package chap03.section5
+
+/**
+ * @author 이승환
+ * @since 2023-05-20
+ */
+fun main() {
+    // 인라인 함수 shortFunc()의 내용이 복사되어 shortFunc으로 들어감.
+    shortFunc(3) { println("First call: $it") }
+    shortFunc(5) { println("Second call: $it") }
+}
+
+inline fun shortFunc(a: Int, out: (Int) -> Unit) {
+    println("Before calling out()")
+    out(a)
+    println("After calling out()")
+}
