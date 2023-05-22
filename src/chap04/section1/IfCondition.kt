30268e572545e170f1d147274c9b1c7fa5a8359d
이승환 <lsh01087674117@gmail.com>
2023-05-22 09:45:48 +0900

    조건문의 표현식 구현해 보기.

---
+++ src/chap04/section1/IfCondition.kt
@@ -0,0 +1,20 @@
+package chap04.section1
+
+/**
+ * @author 이승환
+ * @since 2023-05-22
+ */
+fun main() {
+    val a = 12
+    val b = 7
+
+    val max = if (a > b) {
+        println("a 선택")
+        a // 마지막 식인 a가 반환되어 max에 할당.
+    } else {
+        println("b 선택")
+        b // 마지막 식인 b가 반환되어 max에 할당.
+    }
+
+    println(max)
+}
