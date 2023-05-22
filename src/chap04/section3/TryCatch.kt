6bdd13c7e654666a7c9ad58430602aa452df6aa0
이승환 <lsh01087674117@gmail.com>
2023-05-22 21:28:01 +0900

    0으로 나누었을때 예외 처리하기.

---
+++ src/chap04/section3/TryCatch.kt
@@ -0,0 +1,19 @@
+package chap04.section3
+
+/**
+ * @author 이승환
+ * @since 2023-05-22
+ */
+fun main() {
+    val a = 6
+    val b = 0
+    val c: Int
+
+    try {
+        c = a / b // 0으로 나눔.
+    } catch (e: Exception) {
+        println("Exception is handled.")
+    } finally {
+        println("finally 블록은 반드시 항상 실행됨")
+    }
+}
