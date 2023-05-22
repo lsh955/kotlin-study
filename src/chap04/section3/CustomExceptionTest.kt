f1eb65c89280f5445f9096d699ee40aa9bab534f
이승환 <lsh01087674117@gmail.com>
2023-05-22 21:38:21 +0900

    사용자 예회 클래스 만들어 구현해 보기.

---
+++ src/chap04/section3/CustomExceptionTest.kt
@@ -0,0 +1,24 @@
+package chap04.section3
+
+/**
+ * @author 이승환
+ * @since 2023-05-22
+ */
+class InvalidNameException(message: String): Exception(message) // 사용자 예외 클래스
+
+fun main() {
+    var name = "lsh955" // 숫자가 포함된 이름.
+
+    try {
+        validateName(name)
+    } catch (e: InvalidNameException) { // 숫자가 포함된 예외처리.
+        println(e.message)
+    } catch (e: Exception) { // 기타 예외처리.
+        println(e.message)
+    }
+}
+
+fun validateName(name: String) {
+    if (name.matches(Regex(".*\\d+.*"))) // 이름에 숫자가 포함되어 있으면 예외 발생시킴.
+        throw InvalidNameException("Your name : $name : contains numerals.")
+}
