559064fbc09d4e31155cecb272c1bc165203a8fe
이승환 <lsh01087674117@gmail.com>
2023-05-22 20:38:38 +0900

    주석보강 -> 로직순서가 어떻게 구동되는지 숫자추가

--- src/chap04/section3/InlineLambdaReturn.kt
+++ src/chap04/section3/InlineLambdaReturn.kt
@@ -13,19 +13,19 @@ inline fun inlineLambda(a: Int, b: Int, out: (Int, Int) -> Unit) {
 }
 
 fun retFunc() {
-    println("start of retFunc")
+    println("start of retFunc") // (1)
 
     /**
      * inlineLambda 함수는,
      * 람다식을 매개변수로 사용하는 인라인 함수.
      */
-    inlineLambda(13, 3) { a, b ->
+    inlineLambda(13, 3) { a, b -> // (2)
         val result = a + b
 
-        if(result > 10) // a와 b 인자의 합이 10을 넘는 경우
+        if(result > 10) // (3) a와 b 인자의 합이 10을 넘는 경우
             return // 함수에서 빠져나감.
 
-        println("result: $result") // 10보다 크면 이 문장에서 도달하지 못함.
+        println("result: $result") // (4) 10보다 크면 이 문장에서 도달하지 못함.
     }
-    println("end of retFunc")
+    println("end of retFunc") // (5)
 }
