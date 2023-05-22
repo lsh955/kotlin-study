90e5617cdea722d017f62bafb00f78043cc7b834
이승환 <lsh01087674117@gmail.com>
2023-05-22 11:49:14 +0900

    반복문을 이용하여 삼각형을 출력하는 for문 구현.

---
+++ src/chap04/section2/ForTriangle.kt
@@ -0,0 +1,21 @@
+package chap04.section2
+
+/**
+ * @author 이승환
+ * @since 2023-05-22
+ */
+fun main() {
+    print("Enter the lines: ")
+
+    val n = readLine()!!.toInt() // 콘솔로부터 입력받음.
+
+    for (line in 1..n) {
+        for (space in 1..(n - line))
+            print(" ") // 공백출력.
+
+        for (star in 1..(2 * line - 1))
+            print("*") // 별표출력.
+
+        println() // 개행.
+    }
+}
