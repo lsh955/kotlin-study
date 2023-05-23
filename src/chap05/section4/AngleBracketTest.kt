70ba6a48be67019608c84c1111ae77133cc5dbe5
이승환 <lsh01087674117@gmail.com>
2023-05-23 18:13:08 +0900

    앵글 브래킷을 사용한 이름 중복을 해결구현.

---
+++ src/chap05/section4/AngleBracketTest.kt
@@ -0,0 +1,48 @@
+package chap05.section4
+
+/**
+ * @author 이승환
+ * @since 2023-05-23
+ */
+open class A {
+    open fun f() {
+        println("A Class f()")
+    }
+
+    fun a() {
+        println("A Class a()")
+    }
+}
+
+interface B {
+    fun f() {
+        println("B interface f()") // 인터페이스는 기본적으로 open 임.
+    }
+
+    fun b() {
+        println("B interface b()")
+    }
+}
+
+class C : A(), B { // (1) 쉼표(,)를 사용하여 클래스와 인터페이스를 지정.
+
+    // 컴파일되려면 f()가 오버라이딩되어야 함.
+    override fun f() {
+        println("C Class f()")
+    }
+
+    fun test() {
+        // 이름이 중복되지 않는 경우 그냥 사용가능 하나,
+        f() // (2) 현재 클래스의 f()
+        b() // (3) 인터페이스 B의 b()
+
+        // 중복된 이름인 경우, 앵글 프래킷을 사용 해 처리가능.
+        super<A>.f() // (4) A 클래스의 f()
+        super<B>.f() // (5) B 클래스의 f()
+    }
+}
+
+fun main() {
+    val c = C()
+    c.test()
+}
