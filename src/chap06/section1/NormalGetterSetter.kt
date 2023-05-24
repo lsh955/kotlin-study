78fab5b2a8c90102ee2ae5b3628b6db843cf28e3
이승환 <lsh01087674117@gmail.com>
2023-05-24 19:52:04 +0900

    기본 게터와 세터 지정하기.

---
+++ src/chap06/section1/NormalGetterSetter.kt
@@ -0,0 +1,37 @@
+package chap06.section1
+
+/**
+ * @author 이승환
+ * @since 2023-05-24
+ */
+class User(_id: Int, _name: String, _age: Int) {
+    // 프로퍼티
+    val id: Int = _id
+        get() = field
+
+    var name: String = _name
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var age: Int = _age
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    // value: 세터의 매개변수로 외부로부터 값을 가져옴.
+    // field: 프로퍼티를 참조화는 변수.
+}
+
+fun main() {
+    val user = User(1, "lsh", 30)
+
+    // val 프로퍼티는 값 변경불가.
+    // user.id = 2
+
+    user.age = 31 // 세터
+
+    println("user.age = ${user.age}")
+}
