d54587d33bc6eb87f633667e8e992b93e8d5ef5e
이승환 <lsh01087674117@gmail.com>
2023-05-25 17:49:04 +0900

    코틀린에서 자바의 static 멤버를 사용하기 위한, 자바의 Customer 클래스 구현.

---
+++ src/chap06/section3/Customer.java
@@ -0,0 +1,13 @@
+package chap06.section3;
+
+/**
+ * @author 이승환
+ * @since 2023-05-25
+ */
+public class Customer {
+    public static final String LEVEL = "BASIC";  // static 필드
+
+    public static void login() { // static 메서드
+        System.out.println("Login...");
+    }
+}
