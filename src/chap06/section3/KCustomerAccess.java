6e703978928ed6030a490259ba31a2ad7bd0420f
이승환 <lsh01087674117@gmail.com>
2023-05-25 17:47:53 +0900

    자바에서 코틀린 클래스 접근구현.

---
+++ src/chap06/section3/KCustomerAccess.java
@@ -0,0 +1,23 @@
+package chap06.section3;
+
+/**
+ * @author 이승환
+ * @since 2023-05-25
+ */
+public class KCustomerAccess {
+    public static void main(String[] args) {
+
+        // 코틀린 코드의 KotlinFoo의 멤버를 접근.
+        System.out.println(KCustomer.LEVEL);
+        KCustomer.login(); // 어노테이션을 사용할 때 접근방법.
+        KCustomer.Companion.login(); // 위와 동일한 결과로 어노테이션을 사용하지 않을 때 접근방법.
+
+        // KJob에 대한 객체 생성 후 접근.
+        KJob kjob = KCustomer.JOB;
+        System.out.println(kjob.getTitle());
+
+        // KCostomer를 통한 접근.
+        KCustomer.JOB.setTitle("Accountant");
+        System.out.println(KCustomer.JOB.getTitle());
+    }
+}
