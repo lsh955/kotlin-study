2807d7dc2125c53174746ab869d27fd2c832cd8a
이승환 <lsh01087674117@gmail.com>
2023-05-25 19:08:52 +0900

    object 표현식 사용해보기.

---
+++ src/chap06/section3/ObjectExpressionSuperMan.kt
@@ -0,0 +1,20 @@
+package chap06.section3
+
+/**
+ * @author 이승환
+ * @since 2023-05-25
+ */
+open class Superman() {
+    fun work() = println("Taking photos")
+    fun talk() = println("Talking with people.")
+    open fun fly() = println("Flying in the air.")
+}
+
+fun main() {
+    val pretendedMan = object: Superman() { // object 표현식으로 fly()구현의 재설계.
+        override fun fly() = println("I'm not a real superman. I can't fly!")
+    }
+    pretendedMan.work()
+    pretendedMan.talk()
+    pretendedMan.fly()
+}
