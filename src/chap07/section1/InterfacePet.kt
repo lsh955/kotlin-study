71271c5863747e58b56ff0ee0388c3ebd38602b6
이승환 <lsh01087674117@gmail.com>
2023-05-26 21:16:32 +0900

    Pet을 구현한 Cat class 추가.

--- src/chap07/section1/InterfacePet.kt
+++ src/chap07/section1/InterfacePet.kt
@@ -13,3 +13,10 @@ interface Pet {
         println("Keep patting!")
     }
 }
+
+class Cat(override var category: String) : Pet {
+
+    override fun feeding() {
+        println("Feed the cat a tuna can!")
+    }
+}
