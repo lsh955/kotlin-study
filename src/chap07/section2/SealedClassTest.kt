9a7bbd6c818ff37f2188a4632fa8953561dd24d9
이승환 <lsh01087674117@gmail.com>
2023-05-27 14:15:13 +0900

    상태를 검사하기 위한 추가함수 구현.

--- src/chap07/section2/SealedClassTest.kt
+++ src/chap07/section2/SealedClassTest.kt
@@ -11,3 +11,18 @@ sealed class Result { // 실드 클래스를 선언하는 첫 번째 방법.
 
 class Status: Result()
 class Inside: Result.Success("Status")
+
+fun main() {
+    // Success에 대한 객체 생성
+    val result = Result.Success("Good!")
+    val msg = eval(result)
+    println(msg)
+}
+
+// 상태를 검사하기 위한 함수
+fun eval(result: Result): String = when(result) {
+    is Status -> "in progress"
+    is Result.Success -> result.message
+    is Result.Error -> result.message
+    // 모든 조건을 가지므로 else 가 필요 없다
+}
