cd4d632cf91f2f2f2ad954c21d225009a934bb22
이승환 <lsh01087674117@gmail.com>
2023-05-27 13:57:28 +0900

    메서드에 지역 클래스 추가구현.

--- src/chap07/section2/InnerClassTest.kt
+++ src/chap07/section2/InnerClassTest.kt
@@ -12,9 +12,23 @@ class Smartphone(val model: String) {
             return "${model}: Installed on $cpu with ${size}Gb" // 바깥 클래스의 프로퍼티 접근가능.
         }
     }
+
+    fun powerOn(): String {
+        class Led(val color: String) {  // 이 클래스는 지역 클래스로 사용.
+            fun blink(): String = "Blinking $color on $model" // 외부의 프로퍼티는 접근가능.
+        }
+
+        val powerStatus = Led("Red") // 지역클래스 사용.
+
+        return powerStatus.blink()
+    }
 }
 
 fun main() {
     val mySdcard = Smartphone("S7").ExternalStorage(32)
     println(mySdcard.getInfo())
+
+    val myPhone = Smartphone("Note9")
+    myPhone.ExternalStorage(128)
+    println(myPhone.powerOn())
 }
