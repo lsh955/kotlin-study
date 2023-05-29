0a12f4c3fb1385ed00fc17d14d0fbd79660edab4
이승환 <lsh01087674117@gmail.com>
2023-05-29 16:46:39 +0900

    여러가지 자료형을 지원하면서, 배열에서 특정 요소의 인덱스를 찾아내는 간단한 검색함수 구현.

---
+++ src/chap08/section1/FindIndex.kt
@@ -0,0 +1,24 @@
+package chap08.section1
+
+/**
+ * @author 이승환
+ * @since 2023-05-29
+ */
+fun <T> find(a: Array<T>, Target: T): Int {
+
+    for (i in a.indices) {
+        if (a[i] == Target) {
+            return i
+        }
+    }
+    return -1
+}
+
+fun main() {
+    val arr1: Array<String> = arrayOf("Apple", "Banana", "Cherry", "Durian")
+    val arr2: Array<Int> = arrayOf(1, 2, 3, 4)
+
+    println("arr.indices ${arr1.indices}") // indices는 배열의 유효범위 반환.
+    println(find<String>(arr1, "Cherry")) // 요소 C의 인덱스 찾아내기.
+    println(find(arr2, 2)) // 요소 2의 인덱스 찾아내기.
+}
