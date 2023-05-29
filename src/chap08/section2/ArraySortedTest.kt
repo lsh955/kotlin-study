32c8f56c771128a3ce764d518b61c9505be4b490
이승환 <lsh01087674117@gmail.com>
2023-05-29 22:57:40 +0900

    기본적인 정렬방법 구현.

---
+++ src/chap08/section2/ArraySortedTest.kt
@@ -0,0 +1,35 @@
+package chap08.section2
+
+import java.util.*
+
+/**
+ * @author 이승환
+ * @since 2023-05-29
+ */
+fun main() {
+    val arr = arrayOf(8, 4, 3, 2, 5, 9, 1)
+
+    // 오름차순, 내림차순으로 정렬된 일반 배열로 반환.
+    val sortedNums = arr.sortedArray() // 오른차순.
+    println("ASC: " + Arrays.toString(sortedNums))
+
+    val sortedNumsDesc = arr.sortedArrayDescending() // 내림차순.
+    println("DEC: " + Arrays.toString(sortedNumsDesc))
+
+    // 원본 배열에 대한정렬.
+    arr.sort(1, 3)
+    println("ORI: " + Arrays.toString(arr))
+    arr.sortDescending()
+    println("ORI: " + Arrays.toString(arr))
+
+    // List로 정렬.
+    val listSorted: List<Int> = arr.sorted()
+    val listDesc: List<Int> = arr.sortedDescending()
+    println("LST: " + listSorted)
+    println("LST: " + listDesc)
+
+    // SortBy를 이용한 특정 표현식에 따른 정렬.
+    val items = arrayOf<String>("Dog", "Cat", "Lion", "Kangaroo", "Po")
+    items.sortBy { item -> item.length }
+    println(Arrays.toString(items)) // [Po, Dog, Cat, Lion, Kangaroo]
+}
