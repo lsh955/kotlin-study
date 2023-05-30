7ddc4a378f689837e291e179854e3ae17f4142a3
이승환 <lsh01087674117@gmail.com>
2023-05-30 18:23:59 +0900

    서로다른 자료형 구현내용 추가.

--- src/chap09/section1/ListBasic.kt
+++ src/chap09/section1/ListBasic.kt
@@ -9,6 +9,10 @@ fun main() {
     var numbers: List<Int> = listOf(1, 2, 3, 4, 5)
     var names: List<String> = listOf("one", "two", "three")
 
+    // 서로다른 자료형을 가진요소를 합쳐서 선언도 가능.
+    // 이때, mixedTypes변수는 형식 매개변수가 <Any>를 가진다.
+    var mixedTypes = listOf("Hello", 1, 2.445, 's')
+
     for (name in names) {
         println(name)
     }
