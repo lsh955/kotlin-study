00320f2e4c7225f2faa6cea83255bb0aa1e12192
이승환 <lsh01087674117@gmail.com>
2023-05-30 19:20:53 +0900

    정렬.

--- src/chap09/section1/ImmutableToMutable.kt
+++ src/chap09/section1/ImmutableToMutable.kt
@@ -5,8 +5,7 @@ package chap09.section1
  * @since 2023-05-30
  */
 fun main() {
-     // 기존의 불변형 List를 가변형으로 변경하려면 toMutableList()를 사용할 수 있다.
-
+    // 기존의 불변형 List를 가변형으로 변경하려면 toMutableList()를 사용할 수 있다.
     val names: List<String> = listOf("one", "two", "three") // 불변형 List 초기화.
 
     val mutableNames = names.toMutableList() // 새로운 가변형 List가 만들어짐.
