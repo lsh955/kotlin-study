2bed22877597dc2c92d64e282723d5c0ed5b2a07
이승환 <lsh01087674117@gmail.com>
2023-05-31 16:00:16 +0900

    추가작성 및 정렬.

--- src/chap09/section3/CollectionOperators.kt
+++ src/chap09/section3/CollectionOperators.kt
@@ -10,7 +10,14 @@ fun main() {
     val map1 = mapOf("hi" to 1, "hello" to 2, "Goodbye" to 3)
 
     println(list1 + "four") // + 연산자를 사용한 문자열 요소추가.
+
     println(list2 + 1) // + 연산자를 사용한 정수형 요소추가.
     println(list2 + listOf(5, 6, 7)) // 두 List의 병합.
-    println(list2 - 1) // 요소의 제거
+    println(list2 - 1) // 요소제거.
+    println(list2 - listOf(3, 4, 5)) // 일치하는 요소제거.
+
+    println(map1 + Pair("Bye", 4)) // Pair()를 사용한 map의 추가.
+    println(map1 - "hello") // 일치하는 값 제거.
+    println(map1 + mapOf("Apple" to 4, "Orange" to 5)) // map의 병합
+    println(map1 - listOf("hi", "hello")) // List에 일치하는 값을 map에서 제거
 }
